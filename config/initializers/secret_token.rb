5c18ea7d20386bd799b9854beb443e75e51ae631
Laurent Jolie <laurent@Mythe>
2014-10-14 16:35:55 +0200

    my first commythe

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = '2248dd3b34978defc38a12b2b00f6de3d2f9741bfc02731dd528031a88662612eba9d2381a38470059fcbdd031bc4bf6c0e9eaec503149515ebe7b93edd5ae44'
